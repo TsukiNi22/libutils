c3480c39e5538dba577c9f7bbd64becba382a140
Tsukini <xartania.contact@gmail.com>
2026-07-20 00:07:39 +0200

    feat(attribute): fix define that overwrite other define from included

--- include/utils/attribute/c++14.hpp
+++ include/utils/attribute/c++14.hpp
@@ -1,6 +1,6 @@
 /**************************************************************\
 Edition:
-##  @date 19/05/2026 by @author Tsukini
+##  @date 19/07/2026 by @author Tsukini
 
 File Name:
 ##  @file Attribute-c++14.hpp
@@ -17,41 +17,41 @@ File Description:
     /* DEFINE */
 
     /* attributes */
-    #define nodiscard       __attribute__((warn_unused_result))     // Warn for unused return
-    #define noinline        __attribute__((noinline))               // Cancel any auto inline from the compiler
-    #define unused          __attribute__((unused))                 // Signal an unused variable
-    #define hidden          __attribute__((visibility("hidden")))   // Change the visibility on a shared lib
-    #define ctor            __attribute__((constructor))            // Execute before the main
-    #define dtor            __attribute__((destructor))             // Execute after the main
-    #define fallthrough     __attribute__((fallthrough))            // Ingore warn for no break in switch
-    #ifndef NO_OUTDATED_WARNING
-        #define outdated(info)  [[deprecated(info)]]                // Signal a deprecated function
+    #define _nodiscard      __attribute__((warn_unused_result))     // Warn for unused return
+    #define _noinline       __attribute__((noinline))               // Cancel any auto inline from the compiler
+    #define _unused         __attribute__((unused))                 // Signal an unused variable
+    #define _hidden         __attribute__((visibility("hidden")))   // Change the visibility on a shared lib
+    #define _ctor           __attribute__((constructor))            // Execute before the main
+    #define _dtor           __attribute__((destructor))             // Execute after the main
+    #define _fallthrough    __attribute__((fallthrough))            // Ingore warn for no break in switch
+    #ifndef NO_DEPRECATED_WARNING
+        #define _deprecated(info)   [[deprecated(info)]]            // Signal a deprecated function
     #else
-        #define outdated(info)                                      // Not defined with this flag
+        #define _deprecated(info)                                   // Not defined with this flag
     #endif
 
     /* branch prediction */
-    #define likely          // Not defined in this version
-    #define unlikely        // Not defined in this version
+    #define _likely         // Not defined in this version
+    #define _unlikely       // Not defined in this version
     #define likely_c(c)     __builtin_expect(!!(c), 1)  // Signal a condition that has a bigger probability of appening
     #define unlikely_c(c)   __builtin_expect(!!(c), 0)  // Signal a condition that has a smallest probability of appening
     #define expect(c, v)    __builtin_expect(c, v)      // Signal a condition that has a high probability of having the given value
     
     /* optimisation */
-    #define assume(expr)    __builtin_assume(expr)  // Assume a given expr as true
-    #define cold            __attribute__((cold))   // Signal a function that has a small number of use
-    #define hot             __attribute__((hot))    // Signal a function that has a huge number of use
+    #define _assume(expr)   __builtin_assume(expr)  // Assume a given expr as true
+    #define _cold           __attribute__((cold))   // Signal a function that has a small number of use
+    #define _hot            __attribute__((hot))    // Signal a function that has a huge number of use
 
     /* binary layout */
-    #define remove_address  // Not defined in this version
-    #define remove_padding  __attribute__((packed)) // Remove the memory padding in a struct
-    #define set_padding(n)  alignas(n)              // Set the memory padding in a struct
+    #define _noaddress  // Not defined in this version
+    #define _packed     __attribute__((packed)) // Remove the memory padding in a struct
+    #define _alignas(n) alignas(n)              // Set the memory padding in a struct
 
     /* allocation hints */
-    #define alloc_size(i_size)              __attribute__((alloc_size(i_size)))                 // Signal a malloc of the given size in the argument
-    #define alloc_size_mul(i_mul, i_size)   __attribute__((alloc_size(i_mul, i_size)))          // Signal a malloc of the given size in the argument multiply by another argument
-    #define write_only(i_ptr, i_size)       __attribute__((access(write_only, i_ptr, i_size)))  // Set the mode of a ptr in the argument so that it can only be writed in the limit of the size
-    #define read_only(i_ptr, i_size)        __attribute__((access(read_only, i_ptr, i_size)))   // Set the mode of a ptr in the argument so that it can only be readed in the limit of the size
-    #define nonnull(i_ptr)                  __attribute__((nonnull(i_ptr)))                     // Warn for null on the given argument
+    #define _alloc_size(i_size)             __attribute__((alloc_size(i_size)))                 // Signal a malloc of the given size in the argument
+    #define _alloc_size_mul(i_mul, i_size)  __attribute__((alloc_size(i_mul, i_size)))          // Signal a malloc of the given size in the argument multiply by another argument
+    #define _write_only(i_ptr, i_size)      __attribute__((access(write_only, i_ptr, i_size)))  // Set the mode of a ptr in the argument so that it can only be writed in the limit of the size
+    #define _read_only(i_ptr, i_size)       __attribute__((access(read_only, i_ptr, i_size)))   // Set the mode of a ptr in the argument so that it can only be readed in the limit of the size
+    #define _nonnull(i_ptr)                 __attribute__((nonnull(i_ptr)))                     // Warn on given null value on the given argument
 
 #endif /* ATTRIBUTE_CPP14_H */
--- include/utils/attribute/fallback.hpp
+++ include/utils/attribute/fallback.hpp
@@ -1,6 +1,6 @@
 /**************************************************************\
 Edition:
-##  @date 19/05/2026 by @author Tsukini
+##  @date 19/07/2026 by @author Tsukini
 
 File Name:
 ##  @file Attribute-fallback.hpp
@@ -17,37 +17,37 @@ File Description:
     /* DEFINE */
 
     /* attributes */
-    #define nodiscard
-    #define noinline
-    #define unused
-    #define hidden
-    #define ctor
-    #define dtor
-    #define outdated(info)
-    #define fallthrough
+    #define _nodiscard
+    #define _noinline
+    #define _unused
+    #define _hidden
+    #define _ctor
+    #define _dtor
+    #define _deprecated(info)
+    #define _fallthrough
 
     /* branch prediction */
-    #define likely
-    #define unlikely
+    #define _likely
+    #define _unlikely
     #define likely_c(c)     (c)
     #define unlikely_c(c)   (c)
     #define expect(c, v)    (c)
 
     /* optimisation */
-    #define assume(expr)
-    #define cold
-    #define hot
+    #define _assume(expr)
+    #define _cold
+    #define _hot
 
     /* binary layout */
-    #define remove_address
-    #define remove_padding
-    #define set_padding(n)
+    #define _noaddress
+    #define _packed
+    #define _alignas(n)
 
     /* allocation hints */
-    #define alloc_size(i_size)
-    #define alloc_size_mul(i_mul, i_size)
-    #define write_only(i_ptr, i_size)
-    #define read_only(i_ptr, i_size)
-    #define nonnull(i_ptr)
+    #define _alloc_size(i_size)
+    #define _alloc_size_mul(i_mul, i_size)
+    #define _write_only(i_ptr, i_size)
+    #define _read_only(i_ptr, i_size)
+    #define _nonnull(i_ptr)
 
 #endif /* ATTRIBUTE_FALLBACK_H */
