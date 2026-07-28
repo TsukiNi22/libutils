a8fd4fb529c45f47e2f938ac85234d6470fbad09
Tsukini <xartania.contact@gmail.com>
2026-07-29 00:06:48 +0200

    fix(script): change the link from utils -> libutils

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo curl -o /etc/yum.repos.d/libutils.repo \
-https://tsukini22.github.io/utils/libutils.repo
+https://tsukini22.github.io/libutils/libutils.repo
 
 sudo rpm --import \
-https://tsukini22.github.io/utils/RPM-GPG-KEY-tsukini
+https://tsukini22.github.io/libutils/RPM-GPG-KEY-tsukini
